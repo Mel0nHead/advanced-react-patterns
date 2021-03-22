f0bd0ff532c29fd24ab9ccfb71015e4bc9014974
Alex King <alex.king@deepcrawl.com>
2021-03-22 11:37:57 +0000

    state reducer pattern

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,26 +1,42 @@
-// Prop Collections and Getters
-// http://localhost:3000/isolated/exercise/04.js
-
 import * as React from 'react'
 import {Switch} from '../switch'
+
+function callAllFunctions(...fns) {
+  return (...args) => {
+    fns.forEach(fn => fn(...args))
+  }
+}
 
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
+  const defaultProps = {'aria-pressed': on, onClick: toggle}
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  function getTogglerProps({onClick, ...otherProps} = {}) {
+    return {
+      'aria-pressed': on,
+      ...otherProps,
+      onClick: onClick ? callAllFunctions(onClick, toggle) : toggle,
+    }
+  }
+
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
+
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
